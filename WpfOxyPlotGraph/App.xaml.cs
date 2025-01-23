20ea5ad4d7fd35e7621bd72acdb944ecaf54e30b
jyzayu <moosou30@naver.com>
2025-01-23 11:15:39 +0900

    Add project files.

---
+++ WpfOxyPlotGraph/App.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+using WpfOxyPlotGraph.ViewModels;
+using WpfOxyPlotGraph.Views;
+
+namespace WpfOxyPlotGraph
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public App()
+        {
+            var mainView = new MainView
+            {
+                DataContext = new MainViewModel()
+            };
+
+            mainView.Show();
+        }
+    }
+}
+
+
