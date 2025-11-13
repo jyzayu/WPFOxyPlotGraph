17dd81863755470115ad07192fef11fdab885ba1
jyzayu <moosou30@gmail.com>
2025-11-13 21:57:56 +0900

    feat: DI 컨테이너 객체 등록

--- WpfOxyPlotGraph/App.xaml.cs
+++ WpfOxyPlotGraph/App.xaml.cs
@@ -1,6 +1,9 @@
-﻿using System.Configuration;
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Configuration;
 using System.Data;
 using System.Windows;
+using WpfOxyPlotGraph.Commons;
 using WpfOxyPlotGraph.ViewModels;
 using WpfOxyPlotGraph.Views;
 
@@ -11,16 +14,34 @@ namespace WpfOxyPlotGraph
     /// </summary>
     public partial class App : Application
     {
-        public App()
-        {
-            var mainView = new MainView
-            {
-                DataContext = new MainViewModel()
-            };
+        private ServiceProvider _serviceProvider;
 
-            mainView.Show();
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            var services = new ServiceCollection();
+            ConfigureServices(services);
+            _serviceProvider = services.BuildServiceProvider();
+
+            var mainWindow = _serviceProvider.GetService<MainWindow>();
+            mainWindow?.Show();
+        }
+
+        private void ConfigureServices(ServiceCollection services)
+        {
+            services.AddSingleton<INavigationService>(provider =>
+            {
+                var mainWindow = provider.GetService<MainWindow>();
+                NavigationService navigationService = new NavigationService();
+                navigationService.MainFrame = mainWindow.MainFrame;
+                return navigationService;
+            });
+
+            services.AddTransient<MainViewModel>();
+            services.AddSingleton<MainWindow>();
         }
     }
+
+
 }
 
 
